58212c5960c891d0ab5d17a66028b92e3192682e
aduplii <aduplii@pentalog.com>
2018-02-06 17:20:37 +0200

    Added security and user login functionality.

---
+++ src/AppBundle/Entity/Repositories/UserRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Entity\Repositories;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserRepository extends EntityRepository
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(EntityManager $em, Mapping\ClassMetadata $class)
+    {
+        parent::__construct($em, $class);
+    }
+
+    /**
+     * @param array $attributes, where key = field name, value = field value
+     * @return User
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function create(array $attributes)
+    {
+        $em = $this->getEntityManager();
+
+        $user = new User();
+
+        /*foreach ($attributes as $key => $value) {
+            $setter = "set".ucfirst($key);
+            $user->$setter($value);
+        }*/
+        $user->setUsername($attributes['username']);
+        $user->setPassword($attributes['password']);
+
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repositories\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return self
+     */
+    public function setUsername(?string $username) : self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername() : ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return self
+     */
+    public function setPassword(?string $password) : self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword() : ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return array (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return ['ROLE_ADMIN'];
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
